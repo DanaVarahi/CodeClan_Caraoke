7507580e5aca14a643b84fdc2129470243f2971b
Dana Varahi <dana_varahi@protonmail.com>
2020-10-23 13:59:10 +0100

    Add class Room and test room hasa number

---
+++ src/room.py
@@ -0,0 +1,4 @@
+class Room:
+    def __init__(self, number, playlist):
+        self.number = number
+        self.playlist = playlist
---
+++ tests/room_test.py
@@ -0,0 +1,10 @@
+import unittest
+from src.room import Room
+
+
+class TestRoom(unittest.TestCase):
+    def setUp(self):
+        self.room = Room(1, ['song1', 'song2'])
+
+    def test_room_has_number(self):
+        self.assertEqual(1, self.room.number)
