22073bc53cbfb46b3c64fef4c842890387afedc5
Dana Varahi <dana_varahi@protonmail.com>
2020-10-23 14:45:40 +0100

    Add check in guest and corresponding test

--- src/room.py
+++ src/room.py
@@ -2,6 +2,10 @@ class Room:
     def __init__(self, number):
         self.number = number
         self.playlist = []
+        self.guests = []
 
     def add_song_to_playlist(self, song):
         self.playlist.append(song)
+
+    def check_in_guest_to_room(self, guest):
+        self.guests.append(guest)
--- tests/room_test.py
+++ tests/room_test.py
@@ -6,6 +6,7 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room = Room(1)
         self.new_song = 'song3'
+        self.new_guest = 'guest1'
 
     def test_room_has_number(self):
         self.assertEqual(1, self.room.number)
@@ -16,3 +17,7 @@ class TestRoom(unittest.TestCase):
     def test_add_song_to_playlist(self):
         self.room.add_song_to_playlist(self.new_song)
         self.assertEqual(['song3'], self.room.playlist)
+
+    def test_check_in_guest_to_room(self):
+        self.room.check_in_guest_to_room(self.new_guest)
+        self.assertEqual(['guest1'], self.room.guests)
