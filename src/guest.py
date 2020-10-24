85fd16d49d2518d5d0c3a8305b2dc5c9403bc2e3
Dana Varahi <dana_varahi@protonmail.com>
2020-10-24 12:40:02 +0100

    Add favorite song and corresponding test

--- src/guest.py
+++ src/guest.py
@@ -1,7 +1,8 @@
 class Guest:
-    def __init__(self, name, wallet):
+    def __init__(self, name, wallet, fav_song):
         self.name = name
         self.wallet = wallet
+        self.fav_song = fav_song
 
     def guest_pays_fee(self, fee):
         self.wallet -= fee
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,7 +4,7 @@ from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest = Guest('Dana', 500)
+        self.guest = Guest('Dana', 500, 'Cosmic Woman by Christian Winter')
 
     def test_guest_has_name(self):
         self.assertEqual('Dana', self.guest.name)
@@ -15,3 +15,7 @@ class TestGuest(unittest.TestCase):
     def test_guest_pays_fee(self):
         self.guest.guest_pays_fee(50)
         self.assertEqual(450, self.guest.wallet)
+
+    def test_guest_has_fav_song(self):
+        self.assertEqual('Cosmic Woman by Christian Winter',
+                         self.guest.fav_song)
