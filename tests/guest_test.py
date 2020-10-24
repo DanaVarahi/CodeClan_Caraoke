aea6e88d9429646cbff56b68cb7808730945b225
Dana Varahi <dana_varahi@protonmail.com>
2020-10-24 12:18:58 +0100

    Add wallet to guest and corresponding test

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,7 +4,10 @@ from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest = Guest('Dana')
+        self.guest = Guest('Dana', 500)
 
     def test_guest_has_name(self):
         self.assertEqual('Dana', self.guest.name)
+
+    def test_guest_has_wallet(self):
+        self.assertEqual(500, self.guest.wallet)
